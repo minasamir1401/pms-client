f6f8f19d8b24ff0b9a7c712c41863dac89a2e7c5
Mithaly Admin <admin@mithaly.com>
2026-07-30 22:43:36 +0200

    Shift Hb title left by 30px to align colon with A

--- src/app/view/[id]/CertificatePrintView.tsx
+++ src/app/view/[id]/CertificatePrintView.tsx
@@ -1202,7 +1202,7 @@ export default function CertificatePrintView({ certificate }: CertificatePrintVi
 
           {/* Section 3: Hb Electrophoresis */}
           <div className="mt-1 w-full flex items-start gap-4" style={{ direction: 'ltr', paddingLeft: '55px' }}>
-            <div className="whitespace-nowrap mt-[2px] min-w-fit ml-[-20px]">
+            <div className="whitespace-nowrap mt-[2px] min-w-fit ml-[-50px]">
                 <p style={{ fontWeight: '600', fontSize: '10pt', textDecoration: 'underline', margin: 0 }}>
                     Hb Electrophoresis :
                 </p>
