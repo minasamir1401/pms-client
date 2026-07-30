e9754a69ef120e2db38937775d7f3b8666598359
Mithaly Admin <admin@mithaly.com>
2026-07-30 23:32:38 +0200

    Move National ID right by 30px

--- src/app/view/[id]/CertificatePrintView.tsx
+++ src/app/view/[id]/CertificatePrintView.tsx
@@ -1298,7 +1298,7 @@ export default function CertificatePrintView({ certificate }: CertificatePrintVi
                         {renderLabel("أقر أنا الموقع/الموقعه أدناه : ")}
                         <span className="font-bold text-[9pt]">{editedCert.fullName !== null ? editedCert.fullName : "-"}</span>
                     </p>
-                    <p className="mr-[136px] text-[9pt] m-0 flex items-center gap-1 font-semibold whitespace-nowrap">
+                    <p className="mr-[106px] text-[9pt] m-0 flex items-center gap-1 font-semibold whitespace-nowrap">
                         {renderLabel("رقم القومى : ")}
                         <span className="font-bold text-[9pt]">{editedCert.nationalId !== null ? editedCert.nationalId : "-"}</span>
                     </p>
